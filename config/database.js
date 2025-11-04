a2cc1e6e56be157f8ecc64af9552ce66be7d080e
Suganya Prabagarane <suganyaprabagarane@gmai.com>
2025-11-04 17:37:23 +0200

    update database.js, force IPv4 to connect supabase

--- config/database.js
+++ config/database.js
@@ -39,6 +39,8 @@ module.exports = ({ env }) => {
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
+        keepAlive: true,
+        family: 4,
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
