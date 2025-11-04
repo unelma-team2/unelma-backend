ee87e339658745e60b38ac45a0fbff6216e7d26b
Suganya Prabagarane <suganyaprabagarane@gmai.com>
2025-11-04 18:24:49 +0200

    update plugin.js and .env file to store media in supabase

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'supabase',
+            providerOptions: {
+                supabaseUrl: env('SUPABASE_URL'),
+                supabaseKey: env('SUPABASE_SERVICE_ROLE_KEY'),
+                bucket: env('SUPABASE_BUCKET', 'strapi-uploads'),
+            },
+        },
+    },
+});
