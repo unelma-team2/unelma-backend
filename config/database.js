6127e130a569766b08bc8f0dcf72a3450a5df7cd
Suganya Prabagarane <suganyaprabagarane@gmai.com>
2025-11-20 12:41:40 +0200

    update .env and database.js file

--- config/database.js
+++ config/database.js
@@ -4,24 +4,27 @@ const path = require("path");
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
-    connection: {
+    connection: env("DATABASE_URL", {
       host: env("DATABASE_HOST"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", true) && {
-        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", false),
+      ssl: {
+        rejectUnauthorized: false,
       },
       schema: env("DATABASE_SCHEMA", "public"),
-    },
+    }),
     pool: {
       min: env.int("DATABASE_POOL_MIN", 0),
       max: env.int("DATABASE_POOL_MAX", 5),
     },
     acquireConnectionTimeout: 60000,
-  },
+  }
+
 });
+
+
 
 
 
