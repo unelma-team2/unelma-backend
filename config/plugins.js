5fb94fe5d5103baf893f9aa026613b756f0587a4
Suganya Prabagarane <suganyaprabagarane@gmai.com>
2025-11-20 23:08:01 +0200

    fix the upload stream provider issue

--- config/plugins.js
+++ config/plugins.js
@@ -3,14 +3,20 @@ const path = require("path");
 module.exports = ({ env }) => ({
     upload: {
         config: {
-            provider: "strapi-provider-upload-supabase",
+            provider: "strapi-upload-supabase-provider",
             providerOptions: {
                 apiUrl: env("SUPABASE_URL"),
                 apiKey: env("SUPABASE_SERVICE_ROLE_KEY"),
                 bucket: env("SUPABASE_BUCKET", "strapi_media"),
-                directory: env("SUPABASE_DIRECTORY", undefined),
+                directory: "",
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
             },
         },
     },
 });
+
 
